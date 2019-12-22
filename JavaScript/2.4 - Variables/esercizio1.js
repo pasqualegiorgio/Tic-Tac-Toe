3946168f946fc5c1cf56c98af8119606aff7ce33
pasqualegiorgio <58633329+pasqualegiorgio@users.noreply.github.com>
2019-12-22 09:14:22 +0100

    Old projects for learnign HTML, CSS and JavaScript

---
+++ JavaScript/2.4 - Variables/esercizio1.js
@@ -0,0 +1,14 @@
+/*
+1. Declare two variables: admin and name.
+2. Assign the value "John" to name.
+3. Copy the value from name to admin.
+4. Show the value of admin using alert (must output "John").
+*/
+
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
